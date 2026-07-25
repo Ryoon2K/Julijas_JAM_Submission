6bdc55dc573a68d44ba4616179d56cc8755fa93b
Filipssh <filips.saberts@gmail.com>
2026-07-25 12:11:42 +0300

    Fixed drag and drop

--- Scenes/mouse_pin.gd
+++ Scenes/mouse_pin.gd
@@ -3,19 +3,19 @@ extends Node2D
 
 @onready var mouse_pin: PinJoint2D = $MousePin
 @onready var fake_body: StaticBody2D = $MousePin/FakeBody
-@onready var rigid_body_2d: SoftBody2D = $SoftBody2D
+@onready var soft_body_2d: SoftBody2D = $SoftBody2D
 
 
 var is_dragging = false
+var picked_bone: RigidBody2D
 
 
 func _ready() -> void:
 	# Set the node_a to a static body without a collision, we only need it for the pin effect.
 	mouse_pin.node_a = mouse_pin.get_path_to(fake_body)
-	# Enable input pickable on the rigid body to be able to detect mouse clicks
-	rigid_body_2d.input_pickable = true
 	# Connect the input_event signal to its function
-	rigid_body_2d.input_event.connect(_on_input_event)
+	for bone in soft_body_2d.get_rigid_bodies():
+		bone.rigidbody.input_event.connect(_on_input_event.bind(bone.rigidbody))
 
 
 func _physics_process(delta: float) -> void:
@@ -29,17 +29,18 @@ func _unhandled_input(event: InputEvent) -> void:
 		mouse_pin.node_b = NodePath()
 		is_dragging = false
 		# Reset the angular damp to 0
-		rigid_body_2d.angular_damp = 0
+		picked_bone.angular_damp = 0
 		# Or unlock the rotation of the rigid body with
 #		rigid_body_2d.lock_rotation = false
 
-func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int, bone: RigidBody2D) -> void:
 	# If we aren't dragging and a mouse button press happens then
 	if not is_dragging and event is InputEventMouseButton and event.is_pressed():
 		# Set the node_b to the rigid body that triggered this input event
-		mouse_pin.node_b = mouse_pin.get_path_to(rigid_body_2d)
+		mouse_pin.node_b = mouse_pin.get_path_to(bone)
 		is_dragging = true
+		picked_bone = bone
 		# Up the angular damp to avoid rotating like crazy when moving the mouse
-		rigid_body_2d.angular_damp = 10
+		bone.angular_damp = 10
 		# You can also lock the rotation of the rigid body with
 #		rigid_body_2d.lock_rotation = true

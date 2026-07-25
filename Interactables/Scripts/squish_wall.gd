a50d57ec9ef3b5eaa56cbc1a3db7348aa21f0211
Reinis Kruzbergs <reinisdoesminecraft@gmail.com>
2026-07-25 14:13:43 +0300

    Added UI button and connected in game button to Hydraulic press

--- Interactables/Scripts/squish_wall.gd
+++ Interactables/Scripts/squish_wall.gd
@@ -3,6 +3,9 @@ class_name SquishWall extends Node2D
 @export_group("Dependancies (Ignore)")
 @export var at:AnimationTree
 
+var state:bool = false
+
 func change_state(b:bool):
+	state = b
 	if b: at.set("parameters/Transition/transition_request", "up")
 	else: at.set("parameters/Transition/transition_request", "down")
